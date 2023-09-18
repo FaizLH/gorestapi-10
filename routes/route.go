6d1396905a25087c2141b5505db16d02fbd14591
FaizLH <144418209+FaizLH@users.noreply.github.com>
2023-09-18 17:52:21 +0700

    Update route.go

--- routes/route.go
+++ routes/route.go
@@ -12,7 +12,7 @@ func InitRoute(e *echo.Echo) {
 	e.Use(middleware.Logger())
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, ini tugas rest api golang dengan tema stok item")
+		return c.String(http.StatusOK, 'Hello, ini tugas rest api golang dengan tema stok item. Tambahkan /items pada url')
 	})
 	e.GET("/items", controllers.GetAllItems)
 	e.GET("/items/:id", controllers.GetItemById)
